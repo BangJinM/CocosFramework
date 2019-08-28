9c4b25642d5efc7a57f488188a1e3994a9968e59
mabangjin <mabangjin@outlook.com>
2019-08-28 14:08:02 +0800

    摇杆

--- res/games/start/mediator/loginMediator.lua
+++ res/games/start/mediator/loginMediator.lua
@@ -15,8 +15,12 @@ end
 function loginMediator:handleNotification( notification )
     if notification.name == "open" then 
         local s = display.newScene("NewScene")
-        s:addChild(framework.UIManager.createCsd("views.lobby.Scene"))
+        s:addChild(framework.UIManager.createCsd("views.login.login"))
         display.runScene(s)
+
+        local MainGamePadLayout = require("games.start.layer.GameRockerLayer").create()
+        s:addChild(MainGamePadLayout)
+
     end
 end
 
